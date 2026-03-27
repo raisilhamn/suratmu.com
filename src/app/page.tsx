9d20d8bb810bb87cf0f780350d47075b46e0380f
Rais Ilham <rais.ilham@sevima.id>
2026-03-27 15:49:44 +0700

    Alert

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,15 @@
+import { Alert, AlertDescription } from "@/components/ui/alert";
+
 export default function Home() {
   return (
-    <div className="min-h-screen flex items-center justify-center bg-black">
+    <div className="min-h-screen flex flex-col items-center justify-center gap-6 bg-black px-4">
       {/* eslint-disable-next-line @next/next/no-img-element */}
       <img src="https://c.tenor.com/AfMj1IQE0pMAAAAd/tenor.gif" alt="" />
+      <Alert className="max-w-lg border-white/20 bg-white/5">
+        <AlertDescription className="text-white text-center">
+          Mohon untuk tidak mengirimkan email ke domain ini. Segala email yang dikirimkan ke domain ini akan menjadi milik pemilik domain. Terima kasih.
+        </AlertDescription>
+      </Alert>
     </div>
   );
 }
